778dc677b99a4abbda36c52b490bd69cad3a4f23
Watanabe37R <ryota.watanabe@arsnv.co.jp>
2026-04-20 17:46:31 +0900

    サイト用にいろいろ変更

--- src/main/webapp/js/include.js
+++ src/main/webapp/js/include.js
@@ -19,70 +19,8 @@ btn.addEventListener("click", () => {
 	nav.classList.toggle("hidden");
 });
 
+const menu = document.getElementById("menu");
 
-const messages = ["\n　　＼ﾀﾋﾟｵｶｳﾒｪｽ／\n.　　　　　　　　 |￣\n　　　( ﾟдﾟ )彡.|\n　　　|　ヽﾉヽr┘\n　　　 ＞＞　'Ｔ", "\n\n.　　　　　　　　 |￣\n.　　　(　ﾟдﾟ)　.|\n　　　|　ヽﾉヽr┘\n　　　 ＞＞　'Ｔ"];
-const hoverAA=["　∧　ﾓｸﾞﾓｸﾞ\n　/´｡ `ーァ ﾄﾞｰﾓ\n　{○々ﾟ0l´  FBIﾃﾞｽ\n／　っﾟдﾟ )ｱｯｰ!\n|　　　 / ヽﾉヽ\n＞＞　＞＞"];
-
-
-const aa = document.getElementsByClassName("AAmove")[0];
-
-let index = 0;
-let timer = null;
-
-// アニメ開始
-
-function startAnimation(immediate = false) {
-  if (immediate) {
-    aa.textContent = messages[index]; // ← 即反映
-  }
-
-  timer = setInterval(() => {
-    index = (index + 1) % messages.length;
-    aa.textContent = messages[index];
-  }, 1000);
-}
-
-
-// アニメ停止
-function stopAnimation() {
-  clearInterval(timer);
-}
-
-// 初期表示＆開始
-aa.textContent = messages[0];
-startAnimation();
-
-// マウスオーバー
-aa.addEventListener("mouseenter", () => {
-  stopAnimation();
-  aa.textContent = hoverAA;
+menu.addEventListener("click", () => {
+	menu.classList.toggle("active");
 });
-
-// マウスアウト
-
-aa.addEventListener("mouseleave", () => {
-  stopAnimation();
-  startAnimation(true); // ← ここが肝！
-});
-
-//画像動かし
-const input = document.getElementById("placeOfBirth");
-const container = document.getElementById("birthPlaceImage");
-const img = container.querySelector("img");
-
-// クリックでふんわり表示
-input.addEventListener("click", () => {
-  container.classList.toggle("active");
-});
-
-// 画像ホバーで切替
-img.addEventListener("mouseenter", () => {
-  img.dataset.original = img.src;
-  img.src = img.dataset.hover;
-});
-
-img.addEventListener("mouseleave", () => {
-  img.src = img.dataset.original;
-});
-
-
